79fe5252ff03853565ab9d5ea95205a3e1ac089a
nece001 <nece001@163.com>
2025-10-07 00:16:04 +0800

    修改实现

--- src/Facade/Cache.php
+++ src/Facade/Cache.php
@@ -5,4 +5,69 @@ namespace Nece\Framework\Adapter\Facade;
 use Nece\Framework\Adapter\Contract\Facade\ICache;
 use think\facade\Cache as FacadeCache;
 
-class Cache extends FacadeCache implements ICache {}
+class Cache extends FacadeCache implements ICache
+{
+    /**
+     * @inheritDoc
+     */
+    static public function get(string $key, mixed $default = null): mixed
+    {
+        return parent::get($key, $default);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    static public function set(string $key, mixed $value, null|int|\DateInterval $ttl = null): bool
+    {
+        return parent::set($key, $value, $ttl);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    static public function delete(string $key): bool
+    {
+        return parent::delete($key);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    static public function clear(): bool
+    {
+        return parent::clear();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    static public function getMultiple(iterable $keys, mixed $default = null): iterable
+    {
+        return parent::getMultiple($keys, $default);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    static public function setMultiple(iterable $values, null|int|\DateInterval $ttl = null): bool
+    {
+        return parent::setMultiple($values, $ttl);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    static public function deleteMultiple(iterable $keys): bool
+    {
+        return parent::deleteMultiple($keys);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    static public function has(string $key): bool
+    {
+        return parent::has($key);
+    }
+}
--- src/Facade/Log.php
+++ src/Facade/Log.php
@@ -5,4 +5,141 @@ namespace Nece\Framework\Adapter\Facade;
 use Nece\Framework\Adapter\Contract\Facade\ILog;
 use think\facade\Log as FacadeLog;
 
-class Log extends FacadeLog implements ILog {}
+class Log extends FacadeLog implements ILog
+{
+    /**
+     * 紧急情况
+     *
+     * @author nece001@163.com
+     * @create 2025-10-05 10:43:06
+     *
+     * @param string|\Stringable $message
+     * @param array $context
+     * @return void
+     */
+    static public function emergency(string|\Stringable $message, array $context = []): void
+    {
+        parent::emergency($message, $context);
+    }
+
+    /**
+     * 警告
+     *
+     * @author nece001@163.com
+     * @create 2025-10-05 10:43:22
+     *
+     * @param string|\Stringable $message
+     * @param array $context
+     * @return void
+     */
+    static public function alert(string|\Stringable $message, array $context = []): void
+    {
+        parent::alert($message, $context);
+    }
+
+    /**
+     * 关键错误
+     *
+     * @author nece001@163.com
+     * @create 2025-10-05 10:43:38
+     *
+     * @param string|\Stringable $message
+     * @param array $context
+     * @return void
+     */
+    static public function critical(string|\Stringable $message, array $context = []): void
+    {
+        parent::critical($message, $context);
+    }
+
+    /**
+     * 错误
+     *
+     * @author nece001@163.com
+     * @create 2025-10-05 10:43:54
+     *
+     * @param string|\Stringable $message
+     * @param array $context
+     * @return void
+     */
+    static public function error(string|\Stringable $message, array $context = []): void
+    {
+        parent::error($message, $context);
+    }
+
+    /**
+     * 警告
+     *
+     * @author nece001@163.com
+     * @create 2025-10-05 10:44:09
+     *
+     * @param string|\Stringable $message
+     * @param array $context
+     * @return void
+     */
+    static public function warning(string|\Stringable $message, array $context = []): void
+    {
+        parent::warning($message, $context);
+    }
+
+    /**
+     * 通知
+     *
+     * @author nece001@163.com
+     * @create 2025-10-05 10:44:24
+     *
+     * @param string|\Stringable $message
+     * @param array $context
+     * @return void
+     */
+    static public function notice(string|\Stringable $message, array $context = []): void
+    {
+        parent::notice($message, $context);
+    }
+
+    /**
+     * 信息
+     *
+     * @author nece001@163.com
+     * @create 2025-10-05 10:44:39
+     *
+     * @param string|\Stringable $message
+     * @param array $context
+     * @return void
+     */
+    static public function info(string|\Stringable $message, array $context = []): void
+    {
+        parent::info($message, $context);
+    }
+
+    /**
+     * 调试
+     *
+     * @author nece001@163.com
+     * @create 2025-10-05 10:44:54
+     *
+     * @param string|\Stringable $message
+     * @param array $context
+     * @return void
+     */
+    static public function debug(string|\Stringable $message, array $context = []): void
+    {
+        parent::debug($message, $context);
+    }
+
+    /**
+     * 日志
+     *
+     * @author nece001@163.com
+     * @create 2025-10-05 10:45:09
+     *
+     * @param mixed $level
+     * @param string|\Stringable $message
+     * @param array $context
+     * @return void
+     */
+    static public function log($level, string|\Stringable $message, array $context = []): void
+    {
+        parent::log($level, $message, $context);
+    }
+}
