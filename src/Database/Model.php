8ca0b8658bae27a6f5b74adf36b47cef3fbeb1fd
nece001 <nece001@163.com>
2025-11-22 20:53:15 +0800

    修改结构

--- src/Database/Model.php
+++ src/Database/Model.php
@@ -4,7 +4,6 @@ namespace Nece\Framework\Adapter\Database;
 
 use Nece\Framework\Adapter\Contract\DataBase\IModel;
 use Nece\Framework\Adapter\Contract\DataBase\IQuery;
-use Nece\Framework\Adapter\Contract\DataBase\ScopeManager;
 use think\Model as ThinkModel;
 use think\db\Query as DbQuery;
 
@@ -12,67 +11,33 @@ use think\db\Query as DbQuery;
  * ThinkPHP模型适配类
  *
  * @author nece001@163.com
- * @create 2025-11-09 17:00:42
+ * @create 2025-11-22 17:00:42
  * 
  * @method mixed db()
  */
 abstract class Model extends ThinkModel implements IModel
 {
-    /**
-     * 兼容ThinkPHP的全局查询范围
-     *  - 子类中不要使用这个变量，需要添加的范围方法定义到autoGlobalScope中
-     *
-     * @var array
-     */
-    protected $globalScope = [];
+    private $repository_global_scopes = [];
 
-    /**
-     * 自动应用的全局查询范围的类型
-     *
-     * @var array
-     */
-    protected $globalAutoApplyScopeType = [];
-
-    /**
-     * 自动应用的全局查询范围
-     *
-     * @var array
-     */
-    protected $globalAutoApplyScope = [];
-
-    /**
-     * 构造
-     *
-     * @author nece001@163.com
-     * @create 2025-11-10 19:36:26
-     *
-     * @param array $data
-     */
-    public function __construct(array $data = [])
+    public function newQuery($alias = null): IQuery
     {
-        parent::__construct($data);
-
-        // 添加全局范围
-        $this->globalScope[] = 'GlobalAutoApply';
-        $this->globalScope = array_merge($this->globalScope, $this->globalAutoApplyScope);
+        $query = $this->db(); // 创建一个空的查询对象
+        if ($alias) {
+            $query->alias($alias);
+        }
+        $this->applyRepositoryGlobalScope($query);
+        return new Query($query);
     }
 
-    /**
-     * 统一执行全局范围
-     *
-     * @author nece001@163.com
-     * @create 2025-11-10 19:39:10
-     *
-     * @param DbQuery $query
-     * @return void
-     */
-    public function scopeGlobalAutoApply(DbQuery $query)
+    public function setRepositoryGlobalScope($scopes)
     {
-        foreach ($this->globalAutoApplyScopeType as $type) {
-            $scopes = ScopeManager::getScopes($type);
-            foreach ($scopes as $scope) {
-                $scope($query);
-            }
+        $this->repository_global_scopes = $scopes;
+    }
+
+    public function applyRepositoryGlobalScope($query)
+    {
+        foreach ($this->repository_global_scopes as $scope) {
+            $scope->apply($query);
         }
     }
 
--- src/Database/Repository.php
+++ src/Database/Repository.php
@@ -2,8 +2,9 @@
 
 namespace Nece\Framework\Adapter\Database;
 
-use Nece\Framework\Adapter\Contract\DataBase\IQuery;
+use Nece\Framework\Adapter\Contract\DataBase\IModel;
 use Nece\Framework\Adapter\Contract\DataBase\IRepository;
+use Nece\Framework\Adapter\Contract\DataBase\DbRepository;
 use Nece\Framework\Adapter\Database\Db;
 
 /**
@@ -12,7 +13,7 @@ use Nece\Framework\Adapter\Database\Db;
  * @author nece001@163.com
  * @create 2025-11-09 17:09:11
  */
-abstract class Repository implements IRepository
+abstract class Repository extends DbRepository implements IRepository
 {
     /**
      * @inheritDoc
@@ -46,71 +47,20 @@ abstract class Repository implements IRepository
         Db::rollback();
     }
 
-    public function createModel(): Model
+    protected function createModel(): IModel
     {
-        $class = $this->getModelName();
-        return new $class();
-    }
+        $modelName = $this->getModelName();
+        $model = new $modelName();
 
-    public function find(array $where)
-    {
-        $model = $this->getModelName()::where($where)->select();
-        if (!$model) {
-            return null;
-        }
-        return $this->getEntityName()::buildFromData($model->toArray());
-    }
-
-    public function findById($id)
-    {
-        $model = $this->query()->find($id);
-        if (!$model) {
-            return null;
-        }
-        return $this->getEntityName()::buildFromData($model->toArray());
-    }
-
-    public function delete($entity)
-    {
-        $model_name = $this->getModelName();
-        $model_name::destroy($entity->getId());
-        $entity->emitEvents();
-    }
-
-    public function save($entity): void
-    {
-        $id = $entity->getId();
-        $model = null;
-        $query = $this->query();
-        if ($id) {
-            $model = $query->find($id);
-        }
-        if (!$model) {
-            $model = $this->createModel();
+        // 提取模型可用的全局作用域
+        $available_scopes = [];
+        foreach (self::$model_global_scopes as $name => $scopes) {
+            if ($model instanceof $name) {
+                $available_scopes = array_merge($available_scopes, $scopes);
+            }
         }
 
-        $model->fill($entity->toSaveArray());
-        $model->save();
-        $entity->setId($model->id);
-        $entity->emitEvents();
-    }
-
-    /**
-     * 新建查询
-     *
-     * @author nece001@163.com
-     * @create 2025-11-09 16:42:19
-     *
-     * @param string $alias
-     * @return IQuery
-     */
-    public function query(string $alias = ''): IQuery
-    {
-        $query = $this->createModel()->db(); // 创建一个空的查询对象
-        if ($alias) {
-            $query->alias($alias);
-        }
-
-        return new Query($query);
+        $model->setRepositoryGlobalScope($available_scopes);
+        return $model;
     }
 }
